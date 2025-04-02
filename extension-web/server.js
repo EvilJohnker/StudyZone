d04680ada92d406d0efd363e8b4cb999c312c935
derfeshMariush <darka_mariush@hotmail.com>
2025-04-02 09:31:35 +0200

    Add files via upload

--- extension-web/server.js
+++ extension-web/server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.post("/log", (req, res) => {
   console.log("Received Data:", req.body);
